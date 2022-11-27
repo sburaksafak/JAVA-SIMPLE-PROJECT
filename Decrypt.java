f225da3fe1b78bbafd5b325cc2fbe7f042c5964e
sburaksafak <118398817+sburaksafak@users.noreply.github.com>
2022-11-27 03:58:43 +0300

    Add files via upload
    Write two Java programs that helps users to encrypt and decrypt messages with the Ceasar
    cipher. Your first program should prompt the user to enter a message which is referred to as
    “plaintext" in cryptography. Your program should echo the input and report the encrypted
    message, which is referred to as “ciphertext" in cryptography. Your second program should
    reverse this process, i.e., it should prompt the user to provide a ciphertext and then report the
    corresponding plaintext.
    
    Before starting to write the program, make research on the Ceasar cipher. There are different
    variations of the Caesar cipher, in this homework, you will use the one that uses a right shift
    of 3 to encrypt a message, i.e., a → d , b → e, . . . w → z, x → a, y → b, z → c. The program that
    does the decryption should reverse the process, i.e., decryption is via a left shift of 3, which
    is the same as right shift of 23.

---
+++ Decrypt.java
@@ -0,0 +1,191 @@
+import java.util.Scanner;
+
+public class Decrypt
+
+{ 
+    public static void main(String[]args)
+{
+        String plaintext;
+        char crypted;
+        int c;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter your Crypted: ");
+        plaintext = s.nextLine();
+        System.out.println("Plaintext text: ");
+          
+        
+
+        crypted = plaintext.charAt(0);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+        crypted = plaintext.charAt(1);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(2);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(3);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(4);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(5);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(6);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(7);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(8);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(9);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(10);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(11);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+
+        crypted = plaintext.charAt(12);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+        crypted = plaintext.charAt(13);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<100 && c>=97)    
+                System.out.print((char)(c+23));
+        else 
+            System.out.print((char)(c-3));
+
+
+}
+}
+
+
+
+        
---
+++ Encrypt.java
@@ -0,0 +1,202 @@
+import java.util.Scanner;
+
+public class Encrypt
+
+{ 
+    public static void main(String[]args)
+{
+        String plaintext;
+        char crypted;
+        int c;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter your plaintext: ");
+        plaintext = s.nextLine();
+        System.out.print("Crypted text: ");
+          
+        
+
+        crypted = plaintext.charAt(0);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+        
+
+        crypted = plaintext.charAt(1);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(2);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(3);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(4);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(5);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+            
+        crypted = plaintext.charAt(6);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(7);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+
+        crypted = plaintext.charAt(8);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+
+        crypted = plaintext.charAt(9);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+
+
+        crypted = plaintext.charAt(10);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(11);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(12);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.print((char)(c+3));
+
+
+        crypted = plaintext.charAt(13);
+        c = (int)(crypted);
+
+        if( c==32)        
+             System.out.print(" ");
+        else
+            if( c<123 && c>=120)    
+                System.out.print((char)(c-23));
+        else 
+            System.out.println((char)(c+3));
+
+
+
+
+         
+                
+        
+        
+
+
+
+}
+}
